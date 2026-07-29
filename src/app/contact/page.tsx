e7a75b0454a2ec852c7f1d810c14b9865a0276d6
poondakfai <poondakfai@gmail.com>
2026-07-29 12:10:13 +0700

    [all] fix mobile display, remove contact information.

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -6,11 +6,16 @@ export default function Contact() {
       <h2 className="section-heading">Contact</h2>
       <div className="contact-content">
         <div className="col col-half">
-          <p>Email: huyj2ee@outlook.com</p>
+          <p>Email: n/a</p>
         </div>
         <div className="col col-half">
           <p>GitHub: <a href="https://github.com/huyj2ee" target="_blank" rel="noopener noreferrer">https://github.com/huyj2ee</a></p>
         </div>
+      </div>
+      <div
+        style={{marginLeft: '8px', marginTop: '24px'}}
+      >
+      	Please contact me via freelancer website.
       </div>
     </div>
   );
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -9,6 +9,13 @@ export const metadata: Metadata = {
   description: 'HuyJ2ee blogs, projects and CV',
 };
 
+export const viewport = {
+  width: 'device-width',
+  initialScale: 1,
+  maximumScale: 1,
+  userScalable: false
+};
+
 export default function RootLayout({
   children,
 }: Readonly<{
